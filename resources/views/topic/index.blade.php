03f8e8b09100a4785a03ac235f75bca452ef4c17
Rod <supgeek.rod@gmail.com>
2017-11-08 13:25:07 +0800

    fix topic index page

--- resources/views/topic/index.blade.php
+++ resources/views/topic/index.blade.php
@@ -56,11 +56,11 @@
                         <a class="avatar" href="#"><img class="avatar" src="{{ asset($topic->author->avatar) }}"></a>
                         <div class="pull-left body">
                             <div class="title">
-                                <a href="{{ route('topic.show', $topic->id) }}">{{ $topic->title }}</a>
                                 <span class="info d-none d-sm-inline-block">
                                     {{ $topic->thumb_up_num }} &nbsp; / &nbsp; {{ $topic->comment_num }} &nbsp; / &nbsp; {{ $topic->read_num }}
                                     &nbsp;&nbsp;&nbsp; {{ $topic->created_at->format('m-d') }}
                                 </span>
+                                <a href="{{ route('topic.show', $topic->id) }}">{{ $topic->title }}</a>
                             </div>
                             <div class="content">
                                 {{ mb_substr($topic->content, 0, 150) }}

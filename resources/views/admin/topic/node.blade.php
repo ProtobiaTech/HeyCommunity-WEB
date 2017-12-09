6871338b9d8b3e50bb1d67b29b96ee34d1227ba7
Rod <supgeek.rod@gmail.com>
2017-12-09 12:01:50 +0800

    topic node move feature

--- resources/views/admin/topic/node.blade.php
+++ resources/views/admin/topic/node.blade.php
@@ -12,38 +12,38 @@
         <div class="container">
             <div class="well">
                 <div class="row">
-                    @foreach ($rootNodes as $index => $rootNode)
+                    @foreach ($rootNodes as $rnIndex => $rootNode)
                         <div id="section-node-item" class="col-sm-4">
                             <div class="list-group">
                                 <div class="list-group-item active">
-                                    <span class="text-muted">#{{ $index + 1 }}</span> {{ $rootNode->name }}
+                                    <span class="text-muted">#{{ $rnIndex + 1 }}</span> {{ $rootNode->name }}
                                     <div class="pull-right section-editbox disnone">
                                         <button onclick="nodeDestory('rootNode', 1)" disabled="" class="btn btn-danger btn-xs">
                                             <i class="glyphicon glyphicon-trash"></i>
                                         </button>
                                         &nbsp;&nbsp;
 
-                                        <button onclick="nodeToLeft({{ $rootNode->id }})" class="btn btn-default btn-xs">
+                                        <button onclick="nodeToLeft({{ $rootNode->id }})" class="btn btn-default btn-xs" {{ $rnIndex === 0 ? 'disabled' : '' }}>
                                             <i class="glyphicon glyphicon-arrow-left"></i>
                                         </button>
-                                        <button onclick="nodeToRight({{ $rootNode->id }})" class="btn btn-default btn-xs">
+                                        <button onclick="nodeToRight({{ $rootNode->id }})" class="btn btn-default btn-xs" {{ $rnIndex === $rootNodes->count() - 1 ? 'disabled' : '' }}>
                                             <i class="glyphicon glyphicon-arrow-right"></i>
                                         </button>
                                         <button onclick="nodeRenamePretreatment(1, '默认')" data-toggle="modal" data-target="#nodeRenameModal" class="btn btn-default btn-xs"><i class="glyphicon glyphicon-pencil"></i></button>
                                     </div>
                                 </div>
 
-                                @foreach ($rootNode->children as $node)
+                                @foreach ($rootNode->children as $index => $node)
                                     <div class="list-group-item">
                                         {{ $node->name }}
                                         <div class="pull-right section-editbox disnone">
                                             <button onclick="nodeDestory('node', 2)" class="btn btn-danger btn-xs"><i class="glyphicon glyphicon-trash"></i></button>
                                             &nbsp;&nbsp;
 
-                                            <button onclick="nodeToLeft({{ $node->id }})" class="btn btn-default btn-xs">
+                                            <button onclick="nodeToLeft({{ $node->id }})" class="btn btn-default btn-xs" {{ $index === 0 ? 'disabled' : '' }}>
                                                 <i class="glyphicon glyphicon-arrow-up"></i>
                                             </button>
-                                            <button onclick="nodeToRight({{ $node->id }})" class="btn btn-default btn-xs">
+                                            <button onclick="nodeToRight({{ $node->id }})" class="btn btn-default btn-xs" {{ $index === $rootNode->children->count() - 1 ? 'disabled' : '' }}>
                                                 <i class="glyphicon glyphicon-arrow-down"></i>
                                             </button>
                                             <button onclick="nodeRenamePretreatment(2, '默认1')" data-toggle="modal" data-target="#nodeRenameModal" class="btn btn-default btn-xs"><i class="glyphicon glyphicon-pencil"></i></button>
@@ -51,6 +51,8 @@
                                     </div>
                                 @endforeach
                             </div>
+
+                            <div class="clearfix"></div>
                         </div>
                     @endforeach
                 </div>

8e298224d00c72046268c41ebb889e8861981188
Rod <supgeek.rod@gmail.com>
2017-12-11 23:20:53 +0800

    notice table and model

---
+++ database/migrations/2017_12_11_151407_create_notices_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNoticesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->index()->unsigned()->comment('User ID');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('sender_id')->index()->unsigned()->comment('Sender ID');
+            $table->foreign('sender_id')->references('id')->on('users');
+
+            $table->string('entity_type');
+            $table->integer('entity_id');
+            $table->integer('is_read')->default(0);
+            $table->dateTime('read_at')->nullable();
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notices');
+    }
+}

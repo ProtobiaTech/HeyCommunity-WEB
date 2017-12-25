d5b26394cea71ffca3e03cd211e69d5e64704d14
Rod <supgeek.rod@gmail.com>
2017-12-25 15:59:09 +0800

    user profile form optimization

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -16,19 +16,19 @@
             </nav>
 
             <div class="row">
-                <div class="col-sm-3 m-np">
+                <div class="col-md-3 m-np">
                     @include('user._user-profile-card', ['user' => $user])
                 </div>
 
-                <div class="col-sm-9 m-np">
+                <div class="col-md-9 m-np">
                     <div class="card">
                         <div class="card-body">
                             <form action="{{ route('user.profile-update') }}" method="post">
                                 {{ csrf_field() }}
 
                                 <div class="form-group row">
-                                    <label for="input-nickname" class="col-2 col-form-label">昵称</label>
-                                    <div class="col-10 col-sm-8 col-md-6">
+                                    <label for="input-nickname" class="col-sm-2 col-form-label">昵称</label>
+                                    <div class="col-sm-10 col-md-6">
                                         <input name="nickname" type="text" class="form-control" id="input-nickname" value="{{ old('nickname', $user->nickname) }}">
 
                                         <div class="text-danger">{{ $errors->first('nickname') }}</div>
@@ -36,8 +36,8 @@
                                 </div>
 
                                 <div class="form-group row">
-                                    <label for="input-bio" class="col-2 col-form-label">一句话介绍</label>
-                                    <div class="col-10 col-sm-8 col-md-6">
+                                    <label for="input-bio" class="col-sm-2 col-form-label">一句话介绍</label>
+                                    <div class="col-sm-10 col-md-6">
                                         <input name="bio" type="text" class="form-control" id="input-bio" value="{{ old('bio', $user->bio) }}">
 
                                         <div class="text-danger">{{ $errors->first('bio') }}</div>
@@ -45,8 +45,8 @@
                                 </div>
 
                                 <div class="form-group row">
-                                    <label for="input-gender" class="col-2 col-form-label">性别</label>
-                                    <div class="col-10 col-sm-8 col-md-6">
+                                    <label for="input-gender" class="col-sm-2 col-form-label">性别</label>
+                                    <div class="col-sm-10 col-md-6">
                                         <select name="gender" class="custom-select form-control">
                                             <option selected>请选择性别</option>
                                             @foreach (\App\User::$genders as $value => $name)
@@ -61,8 +61,8 @@
                                 </div>
 
                                 <div class="form-group row">
-                                    <label for="input-phone" class="col-2 col-form-label">电话</label>
-                                    <div class="col-10 col-sm-8 col-md-6">
+                                    <label for="input-phone" class="col-sm-2 col-form-label">电话</label>
+                                    <div class="col-sm-10 col-md-6">
                                         <input name="phone" type="text" class="form-control" id="input-phone" value="{{ old('phone', $user->phone) }}">
 
                                         <div class="text-danger">{{ $errors->first('phone') }}</div>
@@ -70,8 +70,8 @@
                                 </div>
 
                                 <div class="form-group row">
-                                    <label for="input-email" class="col-2 col-form-label">邮箱</label>
-                                    <div class="col-10 col-sm-8 col-md-6">
+                                    <label for="input-email" class="col-sm-2 col-form-label">邮箱</label>
+                                    <div class="col-sm-10 col-md-6">
                                         <input name="email" type="text" class="form-control" id="input-email" value="{{ old('email', $user->email) }}">
 
                                         <div class="text-danger">{{ $errors->first('email') }}</div>
@@ -79,8 +79,8 @@
                                 </div>
 
                                 <div class="form-group row">
-                                    <div class="col-2"></div>
-                                    <div class="col-10 col-sm-8 col-md-6">
+                                    <div class="col-sm-2"></div>
+                                    <div class="col-sm-10 col-md-6">
                                         <button class="btn btn-primary btn-block" type="submit">保存</button>
                                     </div>
                                 </div>

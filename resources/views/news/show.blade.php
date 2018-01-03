51a52f70b69159c768932b29790a112f2e991a79
Rod <supgeek.rod@gmail.com>
2018-01-04 00:35:36 +0800

    optimization news show page

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -4,9 +4,9 @@
     <div id="section-mainbody" class="page-news-index">
         <div class="container pt-4 pb-5">
             <div class="row">
-                <div class="col-md-3"></div>
+                <div class="col-md-2"></div>
 
-                <div class="col-md-6 m-np">
+                <div class="col-md-8 m-np">
                     <div class="card">
                         <div class="card-body">
                             <h5 class="card-title">{{ $news->title }}</h5>
@@ -23,7 +23,9 @@
                                 {!! $news->content !!}
                             </p>
 
+                            <br class="m-inline">
                             <a target="_blank" href="{{ $news->url ?: $news->weburl }}" class="card-link m-inline">访问原文</a>
+                            <br class="m-hide">
                             <a target="_blank" href="{{ $news->weburl ?: $news->url }}" class="card-link m-hide">访问原文</a>
                         </div>
                     </div>

557dea62781ab41cabae39791bff1bf8fb5396e2
Rod <supgeek.rod@gmail.com>
2018-01-07 13:14:30 +0800

    modify ucenter btn alert

--- resources/views/user/ucenter.blade.php
+++ resources/views/user/ucenter.blade.php
@@ -18,9 +18,9 @@
 
                 <div class="row operations">
                     <div class="col-sm-6 text-right d-none d-sm-block">
-                        <a href="" class="btn btn-sm btn-secondary"><i class="fa fa-w fa-user-circle-o"></i> 更换头像</a>
+                        <a onclick="alert('暂不可用')" class="btn btn-sm btn-secondary"><i class="fa fa-w fa-user-circle-o"></i> 更换头像</a>
                         &nbsp;
-                        <a href="" class="btn btn-sm btn-secondary"><i class="fa fa-w fa-picture-o"></i> 更换封面</a>
+                        <a onclick="alert('暂不可用')" class="btn btn-sm btn-secondary"><i class="fa fa-w fa-picture-o"></i> 更换封面</a>
                     </div>
 
                     <div class="col-sm-6 text-left d-none d-sm-block">
@@ -31,8 +31,8 @@
 
                     <div class="col-12 d-block d-sm-none">
                         <div class="btn-group btn-group-sm">
-                            <a href="" class="btn btn-sm btn-secondary">更换头像</a>
-                            <a href="" class="btn btn-sm btn-secondary">更换封面</a>
+                            <a onclick="alert('暂不可用')" class="btn btn-sm btn-secondary">更换头像</a>
+                            <a onclick="alert('暂不可用')" class="btn btn-sm btn-secondary">更换封面</a>
                             <a href="{{ route('user.uhome', $user->id) }}" class="btn btn-sm btn-secondary">我的主页</a>
                             <a href="{{ route('user.profile') }}" class="btn btn-sm btn-secondary">更新资料</a>
                         </div>

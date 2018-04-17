3dba7337c27e09b70cd352e920b41a6c3d86e514
Rod <supgeek.rod@gmail.com>
2018-04-18 02:01:48 +0800

    youth space home page optimizing and complete

--- resources/views/youth-space/home.blade.php
+++ resources/views/youth-space/home.blade.php
@@ -41,13 +41,16 @@
       <div class="collapse navbar-collapse justify-content-end" id="navbar-collapse">
         <ul class="navbar-nav">
           <li class="nav-item">
+            <a class="nav-link" href="#introduction">介绍</a>
+          </li>
+          <li class="nav-item">
             <a class="nav-link" href="#space-and-service">空间与服务</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#moments">精彩瞬间和美好时刻</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">联系我们</a>
+            <a class="nav-link" href="#footer">联系我们</a>
           </li>
         </ul>
       </div>
@@ -66,7 +69,7 @@
     <section class="bottom">
       <div class="container">
         <div class="float-left">
-          5F 青旅式青年生活空间 &nbsp;\&nbsp; 6F 开放式办公空间 &nbsp;\&nbsp; ROOF 露天花园
+          5F 青旅式青年生活空间 &nbsp;\&nbsp; 6F 开放式创作空间 &nbsp;\&nbsp; ROOF 露天花园
         </div>
         <div class="float-right">
           赣州市章贡区新赣南路71号13栋1单元5楼和6楼
@@ -75,45 +78,36 @@
     </section>
   </div>
 
-  <div class="restaurant-intro">
+  <div class="restaurant-intro" id="introduction" style="margin-top:0; padding-top:125px;">
     <h2>
       开放、非营利、公民共治的青年社区
-
-      <br>
-      <small style="display:inline-block; margin-top:8px;">
-        生活 / 办公 / 休闲 / 活动
-      </small>
     </h2>
     <p>
-      在这个日新月异的时代
-      在这个日新月异的时代
-      在这个日新月异的时代
-      在这个日新月异的时代
-      在这个日新月异的时代
-      在这个日新月异的时代
-      在这个日新月异的时代
-      在这个日新月异的时代
-      在这个日新月异的时代
-      在这个日新月异的时代
-      在这个日新月异的时代
+      坐落在赣州老城区，一个由生活空间、创作空间、露天花园组成的服务赣州青年交流协作和学习成长，倡导自在生活的空间和社区。
+      为每一个积极向上的赣州青年开放，你可以在这里生活、工作、休闲娱乐或者是来参加一场活动。
+      作为一个非营利社区，我们将为每一个公民公开社区的收入与支出帐目，收支盈余将投入社区的升级建设。
+      如果你对此有兴趣，写一份申请加入我们，获得公民身份后行使公民义务和权利，共同来治理这个社区
+    </p>
+    <p>
+      现在我们正处于试运营阶段，开放时间为每天的 9:00 - 23:00 <br>
+      欢迎你来参观交流或住宿工作
     </p>
   </div>
 
   <div class="restaurant-grid clearfix">
     <div class="banner">
-      <div class="bg" style="background-image: url('images/restaurant/HNCK5688.jpg');"></div>
+      <div class="bg" style="background-image: url('images/youth-space/exchange.jpg');"></div>
       <section>
         <div class="title">
           交流
         </div>
         <p>
-          开放的心态真诚交流
+          用开放的心态与小伙伴们真诚交流
         </p>
       </section>
     </div>
     <div class="banner">
-      <div class="bg" style="background-image: url('images/restaurant/06.jpg');">
-      </div>
+      <div class="bg" style="background-image: url('images/youth-space/teamwork.jpg');"></div>
       <section>
         <div class="title">
           协作
@@ -124,26 +118,25 @@
       </section>
     </div>
     <div class="banner">
-      <div class="bg" style="background-image: url('images/restaurant/HNCK5665.jpg');">
-      </div>
+      <div class="bg" style="background-image: url('images/youth-space/study.jpg');"></div>
       <section>
         <div class="title">
           学习成长
         </div>
         <p>
-          Available all day
+          stay hungry stay foolish
         </p>
       </section>
     </div>
     <div class="banner">
-      <div class="bg" style="background-image: url('images/restaurant/05.jpg');">
-      </div>
+      <div class="bg" style="background-image: url('images/youth-space/love-life.jpg');"></div>
       <section>
         <div class="title">
-          热爱生活
+          自在生活
         </div>
         <p>
-          Available all day
+          从今日起，做一个幸福的人 <br>
+          住在一所大房子里，楼顶春暖花开
         </p>
       </section>
     </div>
@@ -152,29 +145,32 @@
   <div class="restaurant-menu" id="space-and-service">
     <div class="container">
       <h1>空间与服务</h1>
+      <p class="text-center">
+        270m² 超大空间 &nbsp;&nbsp; 生活 / 工作 / 休闲 / 活动
+      </p>
 
       <div class="row">
-        <div class="col-md-6 offset-md-1 offset-lg-2">
+        <div class="col-md-5 offset-md-1 offset-lg-2">
           <div class="menu-section">
             <h3>5F 生活空间</h3>
             <p>
               青旅式的生活空间，大客厅大厨房让你享受自在生活 <br>
-              1 天至 3 个月住宿服务，46RMB/天 或 650RMB/月 <sup>1</sup> <br>
+              1 天至 3 个月住宿服务，46RMB/天 或 650RMB/月<sup>1</sup> <br>
               撸猫逗狗，唱茶看书晒太阳，认识新朋友
             </p>
           </div>
 
           <div class="menu-section">
-            <h3>6F 办公空间</h3>
+            <h3>6F 创作空间</h3>
             <p>
-              开放式的办公空间，100M 宽带 / 打印机 / 白板 / 会议室 <br>
+              开放式的工作空间，100M 宽带 / 打印机 / 白板 / 会议室 <br>
               支持 10+ 人联合办公，各行各业优秀青年碰撞想法 <br>
               与志同道合的朋友一起创造美好的事物
             </p>
           </div>
         </div>
 
-        <div class="col-md-4">
+        <div class="col-md-5">
           <div class="menu-section">
             <h3>ROOF 露天花园</h3>
             <p>
@@ -186,10 +182,21 @@
           <div class="menu-section">
             <h3>会籍公民</h3>
             <p>
-              154RMB/年（在外游子会籍 54RMB/年）<br>
+              176RMB/年（在外游子 76RMB/年<sup>2</sup>）<br>
               赠送 3 天住宿服务，365 天为你开放 <br>
               享受选举与被选举权，共同治理社区
             </p>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="text-left">
+            <hr class="mt-5">
+            <small>
+              1. 试运营期间住宿打 8 折，还有机会免单 <br>
+              2. 在外游子会籍不赠送住宿服务
+            </small>
           </div>
         </div>
       </div>
@@ -204,79 +211,44 @@
       <div class="gallery">
         <div class="row">
           <div class="col-md-3">
-            <div class="bg-img" style="background-image: url('images/unsplash/xg157kdwbpq-rob-bye.jpg');width: 100%;" data-trigger="zoomerang">
-            </div>
-            <div class="bg-img" style="background-image: url('images/unsplash/qwbobpeoxjo-josh-rose.jpg');width: 100%;" data-trigger="zoomerang">
-            </div>
+            <div class="bg-img" style="background-image:url('images/youth-space/wq1.jpg'); width:100%;" data-trigger="zoomerang"></div>
+            <div class="bg-img" style="background-image:url('images/youth-space/kt1.jpg'); width:100%;" data-trigger="zoomerang"></div>
           </div>
           <div class="col-md-6">
-            <div class="bg-img" style="background-image: url('images/unsplash/rygidtavhkq-dave-lastovskiy.jpg');width: 100%;" data-trigger="zoomerang">
-            </div>
+            <div class="bg-img" style="background-image:url('images/youth-space/kt2.jpg'); width:100%;" data-trigger="zoomerang"></div>
           </div>
           <div class="col-md-3">
-            <div class="bg-img" style="background-image: url('images/unsplash/jkmnm3cincw-yutacar.jpg');width: 100%;" data-trigger="zoomerang">
-            </div>
-            <div class="bg-img" style="background-image: url('images/unsplash/e6hjqab7uea-dan-gold.jpg');width: 100%;" data-trigger="zoomerang">
-            </div>
+            <div class="bg-img" style="background-image:url('images/youth-space/lt1.jpg'); width:100%;" data-trigger="zoomerang"></div>
+            <div class="bg-img" style="background-image:url('images/youth-space/tt1.jpg'); width:100%;" data-trigger="zoomerang"></div>
           </div>
         </div>
       </div>
     </div>
   </div>
 
-  <footer class="footer">
+  <footer class="footer" id="footer">
     <div class="container">
       <div class="row">
         <div class="col-md-4">
           <div class="title">
-            About spacial
+              关于
           </div>
           <ul class="menu">
-            <li>
-              <a href="#">Home Pages</a>
-            </li>
-            <li>
-              <a href="#">Theme Features</a>
-            </li>
-            <li>
-              <a href="#">Services</a>
-            </li>
-            <li>
-              <a href="#">StoreFront</a>
-            </li>
-            <li>
-              <a href="#">Portfolio</a>
-            </li>
+            <li><a href="#introduction">基本介绍</a></li>
+            <li><a href="#space-and-service">空间和服务</a></li>
+            <li><a href="#moments">精彩瞬间和美好时刻</a></li>
+            <li><a href="http://www.hey-ganzhou.com" target="_blank">HEY赣州</a></li>
           </ul>
         </div>
         <div class="col-md-4">
           <div class="title">
             社交媒体
           </div>
+          <div>
+          </div>
           <ul class="menu">
             <li>
-              <a href="#">
-                <i class="fa fa-instagram"></i>
-                Instagram
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i class="fa fa-twitter"></i>
-                Twitter
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i class="fa fa-dribbble"></i>
-                Dribbble
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i class="fa fa-facebook"></i>
-                Facebook
-              </a>
+              <span style="color:#BCC7DF;">暂无</span>
             </li>
           </ul>
         </div>
@@ -286,17 +258,17 @@
           </div>
           <p>
             地址: 赣州市章贡区新赣南路71号13栋1单元5-6楼 <br>
-            电话: <a href="tel:0797-2129400,1">0797-2129400,1</a> <br>
-            E-Mail: <a href="mailto:hi@youthspace.hey-ganzhou.com">hi@youthspace.hey-ganzhou.com</a>
+            电话: <a style="color:#BCC7DF;" href="tel:0797-2129400,1">0797-2129400,1</a> <br>
+            E-Mail: <a style="color:#BCC7DF;" href="mailto:hi@youthspace.hey-ganzhou.com">hi@youthspace.hey-ganzhou.com</a>
           </p>
         </div>
       </div>
       <div class="bottom">
-        <ul>
-          <li><a href="#">Privacy</a></li>
-          <li><a href="#">Legal</a></li>
-          <li><a href="#">Language</a></li>
-        </ul>
+        <div class="text-center" style="color:#bcc7df; padding-top:26px;">
+          <div>试运营期间开放时间为 9:00 - 23:00</div>
+          <div>促进赣州青年交流与协作，为赣州青年人服务</div>
+          <div>&copy; 2018 HEY赣州青年空间 &nbsp; 信息技术支持由进托邦科技提供</div>
+        </div>
       </div>
     </div>
   </footer>

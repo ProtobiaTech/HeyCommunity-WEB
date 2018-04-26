5f29e674766f2e98bcc7b8ba2b19402af6195a34
Rod <supgeek.rod@gmail.com>
2018-04-27 02:47:09 +0800

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -28,10 +28,9 @@ Route::group([], function () {
 
 //
 // User
-Route::group(['prefix' => 'user', 'middleware' => ['wechat.oauth', 'auth.wechat']], function () {
+Route::group(['prefix' => 'user', 'middleware' => []], function () {
     Route::group(['middleware' => 'auth'], function() {
         Route::get('logout', 'UserController@logout')->name('user.logout');
-        Route::get('login-by-wechat-success', 'UserController@loginByWechatSuccess')->name('user.login-by-wechat-success');
     });
 
     Route::group(['middleware' => 'guest'], function() {
@@ -45,10 +44,11 @@ Route::group(['prefix' => 'user', 'middleware' => ['wechat.oauth', 'auth.wechat'
         Route::post('default-login', 'UserController@defaultLoginHandler')->name('user.default-login-handler');
 
         Route::get('login-wechat', 'UserController@loginWechat')->name('user.login-wechat');
-
-        Route::get('login-by-wechat', 'UserController@loginByWechat')->middleware(['wechat.oauth', 'auth.wechat'])->name('user.login-by-wechat');
-        Route::post('login-by-wechat-handler', 'UserController@loginByWechatHandler')->name('user.login-by-wechat-handler');
     });
+
+    Route::get('login-by-wechat', 'UserController@loginByWechat')->middleware(['wechat.oauth', 'auth.wechat'])->name('user.login-by-wechat');
+    Route::post('login-by-wechat-handler', 'UserController@loginByWechatHandler')->name('user.login-by-wechat-handler');
+    Route::get('login-by-wechat-success', 'UserController@loginByWechatSuccess')->name('user.login-by-wechat-success');
 
     Route::middleware(['auth'])->group(function () {
         Route::get('ucenter', 'UserController@ucenter')->name('user.ucenter');

c1818eb62ccf77c0db78489ceae4c8b62c7d9a5d
Rod <supgeek.rod@gmail.com>
2018-05-15 22:35:30 +0800

    modify the page tail

--- resources/views/layouts/_tail.blade.php
+++ resources/views/layouts/_tail.blade.php
@@ -1,9 +1,15 @@
 <div class="card card-link-list">
     <div class="card-body">
-        &copy; 2018 {{ $system->site_title }}
-        <a href="{{ route('site.about') }}">关于</a>
-        <a href="{{ route('site.help') }}">帮助</a>
-        <a href="{{ route('site.terms') }}">用户协议</a>
-        <a href="{{ route('site.privacy') }}">隐私政策</a>
+        <div class="text-nowrap text-overflow">
+            &copy; 2018 {{ $system->site_title }}
+            <small>免费开源的社交软件</small>
+        </div>
+
+        <div>
+            <a class="ml-0" href="{{ route('site.about') }}">关于</a>
+            <a href="{{ route('site.help') }}">帮助</a>
+            <a href="{{ route('site.terms') }}">用户协议</a>
+            <a href="{{ route('site.privacy') }}">隐私政策</a>
+        </div>
     </div>
 </div>
